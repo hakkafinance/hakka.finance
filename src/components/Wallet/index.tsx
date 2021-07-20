3f99a89820c24fba66f57ffdda945cf4e7309e13
gilg4mesh <freedomtomdestiny@gmail.com>
2021-07-21 06:23:52 +0800

    adjustment: use injected connector

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -1,11 +1,31 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
+import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core'
+import { AbstractConnector } from '@web3-react/abstract-connector'
 import images from '../../images/index'
 import styles from './styles'
 // import types from 'prop-types'
 import MyButton from '../Common/MyButton'
+import {
+  injected,
+} from '../../connectors'
+import { shortenAddress } from '../../utils'
+import useENSName from '../../hooks/useENSName'
 
 const Wallet = (props) => {
+  const { activate, account } = useWeb3React()
+  const { ENSName } = useENSName(account ?? undefined)
+  const tryActivation = async (connector: AbstractConnector | undefined) => {
+    connector &&
+      activate(connector, undefined, true).catch((error) => {
+        if (error instanceof UnsupportedChainIdError) {
+          activate(connector)
+        } else {
+          console.error(true)
+        }
+      })
+  }
+
   return (
     <div sx={styles.container}>
       <div sx={styles.chainWrapper}>
@@ -15,7 +35,7 @@ const Wallet = (props) => {
         </span>
       </div>
       <div sx={styles.loginButtonWrapper}>
-        <MyButton click={() => { }}>0x1234…1234</MyButton>
+        <MyButton click={() => tryActivation(injected)}>{account ? ENSName || shortenAddress(account) : 'Connect'}</MyButton>
       </div>
       <img sx={styles.accountIconWrapper} src={images.iconAccount} alt='Account Icon' />
     </div>
