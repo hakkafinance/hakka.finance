fc53909899cb640518ed9190d6635154418e5b21
gilg4mesh <freedomtomdestiny@gmail.com>
2021-08-02 01:28:19 +0800

    fix: Coingecko api error

--- src/hooks/useCoingeckoTokenId.ts
+++ src/hooks/useCoingeckoTokenId.ts
@@ -1,7 +1,11 @@
 import useSWR from 'swr';
 
-export function useCoingeckoTokenId(address: string) {
+export function useCoingeckoTokenId(address: string | null) {
   const fetcher = (url) => fetch(url).then((r) => r.json());
+  if (!address) {
+    return "";
+  }
+  
   const { data } = useSWR(
     `https://api.coingecko.com/api/v3/coins/ethereum/contract/${address}`,
     fetcher,
