58c8436dc81cb7587fb5161ebf3a8751ecdac204
hung-pelith <hung@pelith.com>
2021-08-31 04:50:44 +0800

    adjust: replace double quotes in hoc

--- src/hoc/withWrongNetworkCheckWrapper/index.tsx
+++ src/hoc/withWrongNetworkCheckWrapper/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export interface WrongNetworkCheckWrapperInterface extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   isDisabledWhenNotPrepared?: boolean;
