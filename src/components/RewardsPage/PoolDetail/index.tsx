e9f4d52d52eeff035c72007e720dda169ca06b19
RamanYA <raman@pelith.com>
2021-08-31 22:01:57 +0800

    feat: claim modal

--- src/components/RewardsPage/PoolDetail/index.tsx
+++ src/components/RewardsPage/PoolDetail/index.tsx
@@ -25,6 +25,8 @@ import { useRewardsExit, ExitState } from '../../../hooks/farm/useRewardsExit';
 import { useRewardsDeposit, DepositState } from '../../../hooks/farm/useRewardsDeposit';
 import { useRewardsWithdraw, WithdrawState } from '../../../hooks/farm/useRewardsWithdraw';
 import { useWalletModalToggle } from '../../../state/application/hooks';
+import ClaimModal from '../../ClaimModal';
+import { useClaimModalToggle } from '../../../state/application/hooks';
 import withConnectWalletCheckWrapper from '../../../hoc/withConnectWalletCheckWrapper';
 import withApproveTokenCheckWrapper from '../../../hoc/withApproveTokenCheckWrapper';
 import withWrongNetworkCheckWrapper from '../../../hoc/withWrongNetworkCheckWrapper';
@@ -32,6 +34,7 @@ import withWrongNetworkCheckWrapper from '../../../hoc/withWrongNetworkCheckWrap
 const PoolDetail = ({ pool }) => {
   const { account, chainId, error } = useWeb3React();
   const rewardData = useRewardsData([pool]);
+  const toggleClaimModal = useClaimModalToggle();
   const vestingContract = useVestingContract(VESTING_ADDRESSES[chainId]);
   const vestingValue = useSingleCallResult(
     vestingContract,
@@ -190,152 +193,158 @@ const PoolDetail = ({ pool }) => {
     ) : withdrawButtonRenderer();
 
   return (
-    <div>
-      <a sx={styles.btnBack} href='/farms'>
-        <img src={images.iconBack} />
-        <span>Back</span>
-      </a>
-      <div sx={styles.title}>
-        <p>{REWARD_POOLS[pool].name}</p>
-        <div sx={styles.infoWrapper}>
-          <div sx={styles.infoItem}>
-            {tvl && parseUnits(tvl).gt(Zero)
-              ? <>
-                <span>TVL</span>
-                <span sx={styles.infoValue}> ${tvl} </span>
-              </>
-              : <></>}
-          </div>
-          <div sx={styles.infoItem}>
-            <span>Contract</span>
-            <a sx={styles.contractAddress} target='_blank' href={getEtherscanLink(chainId, pool, 'address')}>{shortenAddress(pool)}</a>
-          </div>
-        </div>
-        <img src={POOL_ASSETES[pool].icon} sx={styles.infoIcon} />
-      </div>
-      <div sx={styles.depositInfoContainer}>
-        <div sx={styles.depositInfoItem}>
-          <p>Deposit</p>
-          <div sx={styles.lpTokenLinkContainer}>
-            <span sx={styles.depositInfoValue}>{REWARD_POOLS[pool].name}</span>
-            <a sx={styles.lpTokenLink} target='_blank' href={REWARD_POOLS[pool].url}>
-              <span> Get Token </span>
-              <img src={images.iconLinkNormal} />
-            </a>
-          </div>
-        </div>
-        <div sx={styles.depositInfoItem}>
-          <p>APR</p>
-          <span sx={styles.depositInfoValue}>
-            {apr}%
-          </span>
-        </div>
-      </div>
-      <div sx={styles.operateArea}>
-        {/* reward */}
-        <div sx={styles.operateCard}>
-          <p>Reward</p>
-          <span>You deposited</span>
-          <div sx={styles.rewardAmountContainer}>
-            {/* if amount === 0 sx={styles.amountIsZero} */}
-            <span>{account ? rewardData.depositBalances[pool]?.toFixed(4) : '-'}</span>
-            <span>{REWARD_POOLS[pool].tokenSymbol}</span>
-          </div>
-          <div sx={styles.rewardInfoContainer}>
-            <div sx={styles.rewardInfoLabelWrapper}>
-              <img src={images.iconClaimableReward} sx={styles.rewardIcon} />
-              <div>
-                <p>Claimable reward</p>
-                {/* if amount !== 0 sx={styles.rewardAmount} */}
-                <p sx={styles.amountIsZero}>{account ? rewardData.earnedBalances[pool]?.toFixed(4) : '-'} HAKKA</p>
-              </div>
+    <>
+      <div>
+        <a sx={styles.btnBack} href='/farms'>
+          <img src={images.iconBack} />
+          <span>Back</span>
+        </a>
+        <div sx={styles.title}>
+          <p>{REWARD_POOLS[pool].name}</p>
+          <div sx={styles.infoWrapper}>
+            <div sx={styles.infoItem}>
+              {tvl && parseUnits(tvl).gt(Zero)
+                ? <>
+                  <span>TVL</span>
+                  <span sx={styles.infoValue}> ${tvl} </span>
+                </>
+                : <></>}
             </div>
-            <div sx={styles.rewardBtn}>
-              <CheckWrongNetworkAndConnectWalletButton
-                styleKit={"green"}
-                isDisabledWhenNotPrepared={true}
-                onClick={claim}
-                isConnected={!!account}
-                connectWallet={toggleWalletModal}
-                disabled={claimState === ClaimState.PENDING}
-                isCorrectNetwork={!isWrongNetwork}
-              >
-                Claim
-              </CheckWrongNetworkAndConnectWalletButton>
+            <div sx={styles.infoItem}>
+              <span>Contract</span>
+              <a sx={styles.contractAddress} target='_blank' href={getEtherscanLink(chainId, pool, 'address')}>{shortenAddress(pool)}</a>
             </div>
           </div>
-          <div sx={styles.rewardInfoContainer}>
-            <div sx={styles.rewardInfoLabelWrapper}>
-              <img src={images.iconWaiting} sx={styles.rewardIcon} />
-              <div>
-                <p>Vesting balance</p>
-                {/* if amount !== 0 remove the style */}
-                <p sx={styles.amountIsZero}>{vestingValueAmount.toFixed(4)} HAKKA</p>
-              </div>
+          <img src={POOL_ASSETES[pool].icon} sx={styles.infoIcon} />
+        </div>
+        <div sx={styles.depositInfoContainer}>
+          <div sx={styles.depositInfoItem}>
+            <p>Deposit</p>
+            <div sx={styles.lpTokenLinkContainer}>
+              <span sx={styles.depositInfoValue}>{REWARD_POOLS[pool].name}</span>
+              <a sx={styles.lpTokenLink} target='_blank' href={REWARD_POOLS[pool].url}>
+                <span> Get Token </span>
+                <img src={images.iconLinkNormal} />
+              </a>
             </div>
-            <a sx={styles.viewBtn} href={'/vesting'}>
-              <span>View</span>
-              <img src={images.iconForward} />
-            </a>
           </div>
-          <div sx={styles.learnMoreLinkWrapper}>
-            <img src={images.iconInform} />
-            <span>
-              Claim means your HAKKA rewards will be locked in vesting contract.
-              <a sx={styles.learnMoreLink} target='_blank' href='https://medium.com/hakkafinance/vesting-contract-9ab2ff24bf76'>learn more</a>
+          <div sx={styles.depositInfoItem}>
+            <p>APR</p>
+            <span sx={styles.depositInfoValue}>
+              {apr}%
             </span>
           </div>
         </div>
-        {/* stake */}
-        <div sx={styles.operateCard}>
-          <p>Stake</p>
-          <div sx={styles.switch}>
-            <div
-              onClick={() => setSwitchPick(SwitchOption.DEPOSIT)}
-              sx={switchPick === SwitchOption.DEPOSIT && styles.switchFocus}
-            >
-              Deposit
+        <div sx={styles.operateArea}>
+          {/* reward */}
+          <div sx={styles.operateCard}>
+            <p>Reward</p>
+            <span>You deposited</span>
+            <div sx={styles.rewardAmountContainer}>
+              {/* if amount === 0 sx={styles.amountIsZero} */}
+              <span>{account ? rewardData.depositBalances[pool]?.toFixed(4) : '-'}</span>
+              <span>{REWARD_POOLS[pool].tokenSymbol}</span>
             </div>
-            <div
-              onClick={() => setSwitchPick(SwitchOption.WITHDRAW)}
-              sx={switchPick === SwitchOption.WITHDRAW && styles.switchFocus}
-            >
-              Withdraw
+            <div sx={styles.rewardInfoContainer}>
+              <div sx={styles.rewardInfoLabelWrapper}>
+                <img src={images.iconClaimableReward} sx={styles.rewardIcon} />
+                <div>
+                  <p>Claimable reward</p>
+                  {/* if amount !== 0 sx={styles.rewardAmount} */}
+                  <p sx={styles.amountIsZero}>{account ? rewardData.earnedBalances[pool]?.toFixed(4) : '-'} HAKKA</p>
+                </div>
+              </div>
+              <div sx={styles.rewardBtn}>
+                <CheckWrongNetworkAndConnectWalletButton
+                  styleKit={"green"}
+                  isDisabledWhenNotPrepared={true}
+                  onClick={toggleClaimModal}
+                  isConnected={!!account}
+                  connectWallet={toggleWalletModal}
+                  disabled={claimState === ClaimState.PENDING}
+                  isCorrectNetwork={!isWrongNetwork}
+                >
+                  Claim
+                </CheckWrongNetworkAndConnectWalletButton>
+              </div>
+            </div>
+            <div sx={styles.rewardInfoContainer}>
+              <div sx={styles.rewardInfoLabelWrapper}>
+                <img src={images.iconWaiting} sx={styles.rewardIcon} />
+                <div>
+                  <p>Vesting balance</p>
+                  {/* if amount !== 0 remove the style */}
+                  <p sx={styles.amountIsZero}>{vestingValueAmount.toFixed(4)} HAKKA</p>
+                </div>
+              </div>
+              <a sx={styles.viewBtn} href={'/vesting'}>
+                <span>View</span>
+                <img src={images.iconForward} />
+              </a>
+            </div>
+            <div sx={styles.learnMoreLinkWrapper}>
+              <img src={images.iconInform} />
+              <span>
+                Claim means your HAKKA rewards will be locked in vesting contract.
+                <a sx={styles.learnMoreLink} target='_blank' href='https://medium.com/hakkafinance/vesting-contract-9ab2ff24bf76'>learn more</a>
+              </span>
             </div>
           </div>
-          <div sx={styles.stakeBalanceContainer}>
-            <span>Amount</span>
-            <span>Balance:{' '}{switchPick === SwitchOption.DEPOSIT ? (tokenBalance?.toExact() || '0.00') : (stakedBalance?.toExact() || '0.00')}</span>
-          </div>
-          <div sx={styles.numericalInputWrapper}>
+          {/* stake */}
+          <div sx={styles.operateCard}>
+            <p>Stake</p>
+            <div sx={styles.switch}>
+              <div
+                onClick={() => setSwitchPick(SwitchOption.DEPOSIT)}
+                sx={switchPick === SwitchOption.DEPOSIT && styles.switchFocus}
+              >
+                Deposit
+              </div>
+              <div
+                onClick={() => setSwitchPick(SwitchOption.WITHDRAW)}
+                sx={switchPick === SwitchOption.WITHDRAW && styles.switchFocus}
+              >
+                Withdraw
+              </div>
+            </div>
+            <div sx={styles.stakeBalanceContainer}>
+              <span>Amount</span>
+              <span>Balance:{' '}{switchPick === SwitchOption.DEPOSIT ? (tokenBalance?.toExact() || '0.00') : (stakedBalance?.toExact() || '0.00')}</span>
+            </div>
+            <div sx={styles.numericalInputWrapper}>
+              {switchPick === SwitchOption.DEPOSIT
+                ? (
+                  <NumericalInputField
+                    value={stakeInputAmount}
+                    onUserInput={setStakeInputAmount}
+                    tokenBalance={tokenBalance}
+                    approve={approve}
+                    approveState={approveState}
+                    setIsCorrectInput={setIsCorrectInput}
+                  />
+                ) : (
+                  <NumericalInputField
+                    value={withdrawInputAmount}
+                    onUserInput={setWithdrawInputAmount}
+                    tokenBalance={stakedBalance}
+                    approve={approve}
+                    approveState={approveState}
+                    setIsCorrectInput={setIsCorrectInput}
+                  />
+                )}
+            </div>
             {switchPick === SwitchOption.DEPOSIT
-              ? (
-                <NumericalInputField
-                  value={stakeInputAmount}
-                  onUserInput={setStakeInputAmount}
-                  tokenBalance={tokenBalance}
-                  approve={approve}
-                  approveState={approveState}
-                  setIsCorrectInput={setIsCorrectInput}
-                />
-              ) : (
-                <NumericalInputField
-                  value={withdrawInputAmount}
-                  onUserInput={setWithdrawInputAmount}
-                  tokenBalance={stakedBalance}
-                  approve={approve}
-                  approveState={approveState}
-                  setIsCorrectInput={setIsCorrectInput}
-                />
-              )}
+              ? depositButtonRenderer()
+              : withdrawButtonContainerRenderer()
+            }
           </div>
-          {switchPick === SwitchOption.DEPOSIT
-            ? depositButtonRenderer()
-            : withdrawButtonContainerRenderer()
-          }
         </div>
       </div>
-    </div>
+      <ClaimModal 
+        claimCallback={claim}
+        claimableReward={account ? rewardData.earnedBalances[pool]?.toFixed(4) : '-'}
+      />
+    </>
   );
 };
 
