f01c73ce3686b835df02b272bba9c31f0e050542
hung-pelith <hung@pelith.com>
2022-01-14 16:31:39 +0800

    update products

--- src/components/ProductScreen/index.tsx
+++ src/components/ProductScreen/index.tsx
@@ -10,32 +10,23 @@ import InfoProduct from './InfoProduct';
 const ProductScreen = (props) => {
   const topArr = [
     {
-      icon: images.iconBlackhole,
-      cardName: 'BlackHoleSwap',
-      info: 'By integrating lending protocols to leverage the excess supply while borrowing on the inadequate side, BlackHoleSwap can therefore process transactions far exceeding its existing liquidity. Compared to other AMMs, BlackHoleSwap provides nearly infinite liquidity with the lowest price slippage, maximizing capital utilization.',
-      whitepaper: 'https://blackholeswap.com/documents/en.pdf',
-      vist: 'https://blackholeswap.com/',
-      id: 1,
-      infoTag: ['DEX', 'Stablecoin'],
-    },
-    {
-      icon: images.icon3FMutual,
-      cardName: '3F Mutual',
-      info: '3F Mutual employs a rainy day fund like mechanism that allows for one to hedge against MakerDAO collapse risk. It exists as neither an option nor a short position of ETH/DAI/MKR. The 3F Mutual rainy day fund-like design means it acts more like collective insurance.',
-      whitepaper: 'https://3fmutual.com/whitepaper.pdf',
-      vist: 'https://3fmutual.com/',
-      id: 2,
-      infoTag: ['Insurance', 'MutualFund'],
-
-    },
-    {
-      icon: images.iconIgain,
-      cardName: 'iGain',
-      info: 'iGain is a decentralized financial instrument protocol that provides the options for investors to hedge/profit/speculate on certain targeting underlying assets with a synthetic, tokenized position.',
-      whitepaper: 'https://hakkafinance.gitbook.io/igain/',
+      icon: images.iconIgainIRS,
+      cardName: 'iGain - Interest Rate Synth',
+      info: 'iGain - Interest Rate Synth (IRS) is an interest rate derivative providing lenders and borrowers a platform to hedge against the risk of future changing interest rates. It empowers users to lock future interest rates, by purchasing Long and Short tokens.',
+      whitepaper: 'https://hakkafinance.gitbook.io/igain/igain-universe/interest-rate-synth',
       vist: 'https://igain.finance/',
-      id: 3,
-      infoTag: ['Options', 'PredictionMarket'],
+      id: 1,
+      infoTag: ['Options', 'Derivatives'],
+      visitButtonContent: 'Visit',
+    },
+    {
+      icon: images.iconIgainIG,
+      cardName: 'iGain - Impermanent Gain',
+      info: 'iGain - Impermanent Gain (IG) can be regarded as the antimatter of impermanent loss. iGain IG consists of two tokens: Long token and Short token, which are empowered to long or short the IL. By holding the Long token, it will be able to offset the IL of holding a certain size of LP position.',
+      whitepaper: 'https://hakkafinance.gitbook.io/igain/igain-universe/impermanent-loss',
+      vist: 'https://igain.finance/',
+      id: 2,
+      infoTag: ['Options', 'Derivatives'],
       visitButtonContent: 'Visit',
     },
     {
@@ -44,15 +35,34 @@ const ProductScreen = (props) => {
       info: 'Hakka Intelligence is a prediction market platform with different mechanisms from general options. There will be no call and put options in this platform but requires the submission of predictions. The payoff is based on the accuracy of the prediction.',
       whitepaper: 'https://medium.com/hakkafinance/hakka-intelligence-handbook-d77a80f44ac6',
       vist: 'https://intelligence.hakka.finance/',
-      id: 4,
+      id: 3,
       infoTag: ['PredictionMarket', 'PricePrediction'],
+    },
+    {
+      icon: images.icon3FMutual,
+      cardName: '3F Mutual',
+      info: '3F Mutual employs a rainy day fund like mechanism that allows for one to hedge against MakerDAO collapse risk. It exists as neither an option nor a short position of ETH/DAI/MKR. The 3F Mutual rainy day fund-like design means it acts more like collective insurance.',
+      whitepaper: 'https://3fmutual.com/whitepaper.pdf',
+      vist: 'https://3fmutual.com/',
+      id: 4,
+      infoTag: ['Insurance', 'MutualFund'],
+
+    },
+    {
+      icon: images.iconBlackhole,
+      cardName: 'BlackHoleSwap',
+      info: 'By integrating lending protocols to leverage the excess supply while borrowing on the inadequate side, BlackHoleSwap can therefore process transactions far exceeding its existing liquidity. Compared to other AMMs, BlackHoleSwap provides nearly infinite liquidity with the lowest price slippage, maximizing capital utilization.',
+      whitepaper: 'https://blackholeswap.com/documents/en.pdf',
+      vist: 'https://blackholeswap.com/',
+      id: 5,
+      infoTag: ['DEX', 'Stablecoin'],
     },
     {
       icon: images.iconDefiHandbook,
       cardName: 'DeFi Handbook',
       info: 'DeFi Handbook make easier for you to dive into DeFi protocols.',
       vist: 'https://defihandbook.cc/',
-      id: 5,
+      id: 6,
       infoTag: ['Dev', 'Tool'],
     },
     {
@@ -61,7 +71,7 @@ const ProductScreen = (props) => {
       info: 'Hakka Harvester is a yield farming portal for those who are chasing farms with high-APY and show more willingness to hold $HAKKA instead of other tokens.',
       whitepaper: 'https://medium.com/hakkafinance/hakka-harvester-born-for-chad-hakka-farmers-83c37f0170b9',
       vist: 'https://harvesters.hakka.finance/pools',
-      id: 6,
+      id: 7,
       infoTag: ['Aggregator', 'YieldFarming'],
     },
   ];
@@ -180,6 +190,38 @@ const ProductScreen = (props) => {
         </Box>
       ));
     }
+    return topArr.slice(6, 9).map((item) => (
+      <Box key={item.cardName} onClick={handleGetSelectedCard(item)}>
+        <CardPorduct
+          selectedCard={selectedCard}
+          onShowInfo={handleShowInfo}
+          cardData={item}
+        />
+      </Box>
+    ));
+  };
+
+  const renderCardProductResponsive3 = () => {
+    if (screenWidth < 1195) {
+      return topArr.slice(6, 8).map((item) => (
+        <Box key={item.cardName} onClick={handleGetSelectedCard(item)}>
+          <CardPorduct
+            selectedCard={selectedCard}
+            onShowInfo={handleShowInfo}
+            cardData={item}
+          />
+        </Box>
+      ));
+    }
+    return topArr.slice(9, 12).map((item) => (
+      <Box key={item.cardName} onClick={handleGetSelectedCard(item)}>
+        <CardPorduct
+          selectedCard={selectedCard}
+          onShowInfo={handleShowInfo}
+          cardData={item}
+        />
+      </Box>
+    ));
   };
 
   const renderComingProduct = () => botArr.map((item) => (
@@ -245,7 +287,24 @@ const ProductScreen = (props) => {
         </Box>
 
         <Box>
-          {(screenWidth < 1195 && selectedCardId >= 5 && selectedCardId <= 6)
+          {selectedCardId > (screenWidth < 1195 ? 4 : 6) && selectedCardId < (screenWidth < 1195 ? 7 : 9)
+            ? (
+              <InfoProduct
+                onClose={handleCloseInfo}
+                dataInfo={dataInfo}
+                isShowInfoProduct={isShowInfoProduct}
+                infoTag={selectedInfoTag}
+              />
+            )
+            : ''}
+        </Box>
+
+        <Box sx={styles.responsive_cards} mt="48px">
+          {renderCardProductResponsive3()}
+        </Box>
+
+        <Box>
+          {screenWidth < 1195 && selectedCardId > 6
             ? (
               <InfoProduct
                 onClose={handleCloseInfo}
