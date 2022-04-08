be65de68dd999d2a7729409369b1c38663f82dca
RamanYA <raman@pelith.com>
2022-04-08 15:36:54 +0800

    adjust: tokenMetrics config

--- src/components/HomeScreen/component/TokenMetrics/index.tsx
+++ src/components/HomeScreen/component/TokenMetrics/index.tsx
@@ -9,6 +9,7 @@ import { CoinComponent } from '../../../../components/Common';
 import styles from './styles';
 import AddToMetamaskBtn from '../../../AddToMetamaskBtn';
 import { useWeb3React } from '@web3-react/core';
+import { chainsInfo, tokenMetrics } from '../../../../constants/tokenMetrics';
 
 const TokenMetricContent = (props) => {
   const { tokenMetrics } = props;
@@ -107,63 +108,6 @@ const TokenMetricContent = (props) => {
 
 function TokenMetrics(props) {
   const { chainId } = useWeb3React();
-  const chains = [
-    {
-      id: 'eth',
-      chainId: 1,
-      imgChain: 'iconEthereum',
-      chainName: 'Ethereum Mainnet',
-      imgBg: '#f2f2f2',
-    },
-    {
-      id: 'bsc',
-      chainId: 56,
-      imgChain: 'iconBinanceGold',
-      chainName: 'Binance Smart Chain',
-      imgBg: '#fcf7de',
-    },
-    {
-      id: 'polygon',
-      chainId: 137,
-      imgChain: 'iconPolygon',
-      chainName: 'Polygon Network',
-      imgBg: '#f2ebff',
-    },
-  ];
-
-  const tokenMetrics = [
-    {
-      id: 'eth',
-      chainId: 1,
-      shortName: 'Ethereum',
-      name: 'Hakka Finance (HAKKA)',
-      type: 'ERC-20',
-      address: '0x0E29e5AbbB5FD88e28b2d355774e73BD47dE3bcd',
-      addressLink: 'https://etherscan.io/token/0x0E29e5AbbB5FD88e28b2d355774e73BD47dE3bcd',
-      addressIcon: images.iconEtherscan,
-    },
-    {
-      id: 'bsc',
-      chainId: 56,
-      shortName: 'BSC',
-      name: ' Hakka Finance on xDai on BSC (HAKKA)',
-      type: 'BEP-20',
-      address: '0x1D1eb8E8293222e1a29d2C0E4cE6C0Acfd89AaaC',
-      addressLink: 'https://bscscan.com/token/0x1D1eb8E8293222e1a29d2C0E4cE6C0Acfd89AaaC',
-      addressIcon: images.iconBSCScan,
-    },
-    {
-      id: 'polygon',
-      chainId: 137,
-      shortName: 'Polygon',
-      name: ' Hakka Finance (HAKKA)',
-      type: 'ERC-20',
-      address: '0x978338A9d2d0aa2fF388d3dc98b9bF25bfF5efB4',
-      addressLink: 'https://polygonscan.com/token/0x978338A9d2d0aa2fF388d3dc98b9bF25bfF5efB4',
-      addressIcon: images.iconPolygon,
-    },
-  ];
-
   const [selectedCoin, setSelectedCoin] = useState('eth');
   const [selectedTokenMetric, setSelectedTokenMetric] = useState(tokenMetrics[0]);
 
@@ -175,14 +119,14 @@ function TokenMetrics(props) {
   };
 
   useEffect(() => {
-    const currentChain = chains.find((chain) => 
+    const currentChain = chainsInfo.find((chain) => 
       chain.chainId === chainId
     )
     handleSelectCoin(currentChain.id)();
   }, [chainId]);
 
   // render
-  const renderChain = () => chains.map((chain) => (
+  const renderChain = () => chainsInfo.map((chain) => (
     <Flex
       sx={selectedCoin === chain.id ? styles.chainContent_active : styles.chainContent}
       className="border-top-active"
