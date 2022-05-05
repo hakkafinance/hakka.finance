b9c00b541af46561d197068c8cc9ea78f8e2baf2
paul li <paul.li@pelith.com>
2022-05-05 19:53:33 +0800

    fix: locale date

--- src/utils/dateRangeCal.ts
+++ src/utils/dateRangeCal.ts
@@ -4,5 +4,9 @@ export const dateRangeCal = (duration = 0, maximumDuration = 0, minimumDuration 
 
 export function getExpectedDay(base = new Date, totalTime = 0) {
   const date = new Date(base.getTime() + totalTime);
-  return date.getDate();
+  return date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
 }
