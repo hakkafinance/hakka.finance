8e17dabd8da9820bc220076988ef371d02459b2e
paul li <paul.li@pelith.com>
2022-05-17 22:51:25 +0800

    adjust: format + add address zero detect

--- src/hooks/useVotingPower.ts
+++ src/hooks/useVotingPower.ts
@@ -1,4 +1,8 @@
-import { Contract as MulticallContract, Provider as MulticallProvider } from '@pelith/ethers-multicall';
+import { AddressZero, Zero } from '@ethersproject/constants';
+import {
+  Contract as MulticallContract,
+  Provider as MulticallProvider,
+} from '@pelith/ethers-multicall';
 import { useWeb3React } from '@web3-react/core';
 import { ChainDataFetchingState, NEW_SHAKKA_ADDRESSES } from '../constants';
 import debounce from 'lodash.debounce';
@@ -10,64 +14,97 @@ import { useBlockNumber } from '../state/application/hooks';
 import STAKING_ABI from '../constants/abis/shakka.json';
 
 export type VotingPowerType = {
-    [chainId in ChainId]: BigNumber; 
-  };
+  [chainId in ChainId]: BigNumber;
+};
 
 export default function useVotingPower(): {
   votingPowerInfo: VotingPowerType;
   fetchVotingPowerState: ChainDataFetchingState;
-  } {
-    const { account } = useWeb3React();
-    const latestBlockNumber = useBlockNumber();
-    const [votingPowerInfo, setVotingPowerInfo] = useState<VotingPowerType>();
-    const [transactionSuccess, setTransactionSuccess] = useState(false);
-  
-    const fetchDataState: ChainDataFetchingState = useMemo(() => {
-      return transactionSuccess ? ChainDataFetchingState.SUCCESS : ChainDataFetchingState.LOADING;
-    }, [transactionSuccess]);
- 
-    const providers = useMemo(() => {
-      const ethProvider = new JsonRpcProvider(process.env.REACT_APP_NETWORK_URL);
-      const bscProvider = new JsonRpcProvider(process.env.REACT_APP_BSC_NETWORK_URL);
-      const polygonProvider = new JsonRpcProvider(process.env.REACT_APP_POLYGON_NETWORK_URL);
-      const kovanProvider = new JsonRpcProvider(process.env.REACT_APP_KOVAN_NETWORK_URL);
-      return {[ChainId.MAINNET]: ethProvider, [ChainId.BSC]: bscProvider, [ChainId.POLYGON]: polygonProvider, [ChainId.KOVAN]: kovanProvider};
-    }, [])
+} {
+  const { account } = useWeb3React();
+  const latestBlockNumber = useBlockNumber();
+  const [votingPowerInfo, setVotingPowerInfo] = useState<VotingPowerType>(
+    {} as VotingPowerType
+  );
+  const [transactionSuccess, setTransactionSuccess] = useState(false);
 
-    const getVotingPower = useCallback(async (chainId: ChainId) => {
-      const multicallProvider = new MulticallProvider(providers[chainId], chainId);
-      const sHakkaContract = new MulticallContract(NEW_SHAKKA_ADDRESSES[chainId], STAKING_ABI);
-      const [ votingPower ] = await multicallProvider.all([sHakkaContract.votingPower(account)]);
-      return votingPower;
-    }, []);
+  const fetchDataState: ChainDataFetchingState = useMemo(() => {
+    return transactionSuccess
+      ? ChainDataFetchingState.SUCCESS
+      : ChainDataFetchingState.LOADING;
+  }, [transactionSuccess]);
 
-    const fetchVotingPower = useCallback(async () => {
-      setTransactionSuccess(false);
-      try {
-        const [ethVotingPower, bscVotingPower, polygonVotingPower, kovanVotingPower] = await Promise.all([
-          getVotingPower(ChainId.MAINNET),
-          getVotingPower(ChainId.BSC),
-          getVotingPower(ChainId.POLYGON),
-          getVotingPower(ChainId.KOVAN),
-        ]);
-        
-        setVotingPowerInfo({
-          [ChainId.MAINNET]: ethVotingPower,
-          [ChainId.BSC]: bscVotingPower,
-          [ChainId.POLYGON]: polygonVotingPower,
-          [ChainId.KOVAN]: kovanVotingPower});
-        setTransactionSuccess(true);
-      } catch (e) {
-        console.log(e);
-        console.log('fetch user voting power error');
-      }
-    }, []);
-  
-    const debouncedFetchVotingPower = useMemo(() => debounce(fetchVotingPower, 200), []);
-  
-    useEffect(() => {
-        debouncedFetchVotingPower();
-    }, [latestBlockNumber]);
-  
-    return { votingPowerInfo, fetchVotingPowerState: fetchDataState };
-  }
+  const providers = useMemo(() => {
+    const ethProvider = new JsonRpcProvider(process.env.REACT_APP_NETWORK_URL);
+    const bscProvider = new JsonRpcProvider(
+      process.env.REACT_APP_BSC_NETWORK_URL
+    );
+    const polygonProvider = new JsonRpcProvider(
+      process.env.REACT_APP_POLYGON_NETWORK_URL
+    );
+    const kovanProvider = new JsonRpcProvider(
+      process.env.REACT_APP_KOVAN_NETWORK_URL
+    );
+    return {
+      [ChainId.MAINNET]: ethProvider,
+      [ChainId.BSC]: bscProvider,
+      [ChainId.POLYGON]: polygonProvider,
+      [ChainId.KOVAN]: kovanProvider,
+    };
+  }, []);
+
+  const getVotingPower = useCallback(async (chainId: ChainId) => {
+    if (NEW_SHAKKA_ADDRESSES[chainId] === AddressZero) return Zero;
+    const multicallProvider = new MulticallProvider(
+      providers[chainId],
+      chainId
+    );
+    const sHakkaContract = new MulticallContract(
+      NEW_SHAKKA_ADDRESSES[chainId],
+      STAKING_ABI
+    )
+    const [votingPower] = await multicallProvider.all([
+      sHakkaContract.votingPower(account),
+    ]);
+    return votingPower;
+  }, []);
+
+  const fetchVotingPower = useCallback(async () => {
+    setTransactionSuccess(false);
+    try {
+      const [
+        ethVotingPower,
+        bscVotingPower,
+        polygonVotingPower,
+        kovanVotingPower,
+      ] = await Promise.all([
+        getVotingPower(ChainId.MAINNET),
+        getVotingPower(ChainId.BSC),
+        getVotingPower(ChainId.POLYGON),
+        getVotingPower(ChainId.KOVAN),
+      ]);
+
+      setVotingPowerInfo({
+        [ChainId.MAINNET]: ethVotingPower,
+        [ChainId.BSC]: bscVotingPower,
+        [ChainId.POLYGON]: polygonVotingPower,
+        [ChainId.KOVAN]: kovanVotingPower,
+      });
+      setTransactionSuccess(true);
+    } catch (e) {
+      console.log(e);
+      console.log('fetch user voting power error');
+    }
+  }, []);
+
+  const debouncedFetchVotingPower = useMemo(
+    () => debounce(fetchVotingPower, 200),
+    []
+  );
+
+  useEffect(() => {
+    debouncedFetchVotingPower();
+  }, [latestBlockNumber]);
+
+  return { votingPowerInfo, fetchVotingPowerState: fetchDataState };
+}
