83c2cc16e7b4ae82b503929673f52047d025db59
paul li <paul.li@pelith.com>
2022-05-19 03:41:12 +0800

    fix: restake approved, vault type

--- src/components/RestakeModal/index.tsx
+++ src/components/RestakeModal/index.tsx
@@ -13,33 +13,53 @@ import styles, { headerWrapper } from './styles';
 import { MyButton } from '../Common';
 import NumericalInputField from '../NumericalInputField';
 import { ApprovalState, useTokenApprove } from '../../hooks/useTokenApprove';
-import VotingPowerSection from '../StakingPage/StakingPanel/VotingPowerSection'
+import VotingPowerSection from '../StakingPage/StakingPanel/VotingPowerSection';
 import LockPeriodOptions from '../StakingPage/StakingPanel/LockPeriodOptions.tsx';
 import { useTokenBalance } from '../../state/wallet/hooks';
 import { ChainId, HAKKA, NEW_SHAKKA_ADDRESSES } from '../../constants';
-import useHakkaRestake, { RestakeState } from '../../hooks/staking/useHakkaRestake';
+import useHakkaRestake, {
+  RestakeState,
+} from '../../hooks/staking/useHakkaRestake';
 import { restakeReceivedAmount } from '../../utils/stakeReceivedAmount';
 import { transferToYear } from '../../utils';
+import withApproveTokenCheckWrapper from '../../hoc/withApproveTokenCheckWrapper';
+import withWrongNetworkCheckWrapper from '../../hoc/withWrongNetworkCheckWrapper';
+import withConnectWalletCheckWrapper from '../../hoc/withConnectWalletCheckWrapper';
+import { VaultType } from '../../hooks/staking/useStakingVault';
 interface RestakeModalInterface {
   index: number;
-  vaults?: any[];
+  vaults?: VaultType[];
   account: string;
   chainId: ChainId;
+  isCorrectNetwork: boolean;
+  toggleWalletModal(): void;
 }
 
 interface StayTheSameSwitchWithTitleType {
   title?: string;
   switchState: boolean;
   handleSwitchChange?: () => void;
-  isDisable?: boolean
-};
+  isDisable?: boolean;
+}
 
-const StayTheSameSwitchWithTitle = ({ title, switchState, handleSwitchChange, isDisable }: StayTheSameSwitchWithTitleType) => {
+const StayTheSameSwitchWithTitle = ({
+  title,
+  switchState,
+  handleSwitchChange,
+  isDisable,
+}: StayTheSameSwitchWithTitleType) => {
   return (
     <div sx={headerWrapper}>
       <p>{title}</p>
       <div>
-        <Switch disabled={isDisable} id="stake-position-switch" className="switch" label="Stay the same" checked={switchState} onChange={handleSwitchChange}></Switch>
+        <Switch
+          disabled={isDisable}
+          id="stake-position-switch"
+          className="switch"
+          label="Stay the same"
+          checked={switchState}
+          onChange={handleSwitchChange}
+        ></Switch>
       </div>
     </div>
   );
@@ -48,7 +68,18 @@ const StayTheSameSwitchWithTitle = ({ title, switchState, handleSwitchChange, is
 // 365.25 days a year
 const secOfFourYear = 31557600 * 4;
 
-const RestakeModal = ({ vaults, index, account, chainId }: RestakeModalInterface) => {
+const RestakeButton = withApproveTokenCheckWrapper(
+  withWrongNetworkCheckWrapper(withConnectWalletCheckWrapper(MyButton))
+);
+
+const RestakeModal = ({
+  vaults,
+  index,
+  account,
+  chainId,
+  toggleWalletModal,
+  isCorrectNetwork,
+}: RestakeModalInterface) => {
   const restakeModalOpen = useRestakeModalOpen();
   const toggleRestakeModal = useRestakeModalToggle();
   const [inputAmount, setInputAmount] = useState('0');
@@ -56,7 +87,7 @@ const RestakeModal = ({ vaults, index, account, chainId }: RestakeModalInterface
   const [isCorrectInput, setIsCorrectInput] = useState<boolean>(true);
   const [isKeepAmountTheSame, setIsKeepAmountTheSame] = useState(false);
   const [isKeepPeriodTheSame, setIsKeepPeriodTheSame] = useState(false);
-  const vault = vaults[index]?.result
+  const vault = vaults[index];
   const hakkaBalance = useTokenBalance(account, HAKKA[chainId]);
 
   const handleKeepAmountTheSame = useCallback(() => {
@@ -80,15 +111,20 @@ const RestakeModal = ({ vaults, index, account, chainId }: RestakeModalInterface
     NEW_SHAKKA_ADDRESSES[chainId],
     index,
     isKeepAmountTheSame ? vault?.hakkaAmount : parseUnits(inputAmount, 18),
-    isKeepPeriodTheSame ? timeLeft : period,
-  )
+    isKeepPeriodTheSame ? timeLeft : period
+  );
 
   const periodYear = transferToYear(period);
   const trialInputAmount = isKeepAmountTheSame ? '0' : inputAmount;
   const trialPeriod = isKeepPeriodTheSame ? timeLeft.toString() : periodYear;
 
   // TODO: stakingRate is not ready
-  const [receivedSHakkaAmount, additionalSHakkaAmount] = restakeReceivedAmount(trialInputAmount, trialPeriod, WeiPerEther, vault);
+  const [receivedSHakkaAmount, additionalSHakkaAmount] = restakeReceivedAmount(
+    trialInputAmount,
+    trialPeriod,
+    WeiPerEther,
+    vault
+  );
 
   const [approveState, approve] = useTokenApprove(
     HAKKA[chainId],
@@ -96,31 +132,33 @@ const RestakeModal = ({ vaults, index, account, chainId }: RestakeModalInterface
     inputAmount
   );
 
-  const isDisable = (parseFloat(inputAmount) !== 0 && !isCorrectInput)
-    || restakeState === RestakeState.PENDING 
-    || (isKeepPeriodTheSame && isKeepAmountTheSame) 
-    || parseFloat(additionalSHakkaAmount) <= 0 
-    || isLeftTimeLessThan30Mins;
+  const isDisable =
+    (parseFloat(inputAmount) !== 0 && !isCorrectInput) ||
+    restakeState === RestakeState.PENDING ||
+    (isKeepPeriodTheSame && isKeepAmountTheSame) ||
+    parseFloat(additionalSHakkaAmount) <= 0 ||
+    isLeftTimeLessThan30Mins;
 
-  const isRestakePending =  restakeState === RestakeState.PENDING;
+  const isRestakePending = restakeState === RestakeState.PENDING;
 
   const btnContent = isRestakePending
-    ? 'Pending' 
-    : isLeftTimeLessThan30Mins 
+    ? 'Pending'
+    : isLeftTimeLessThan30Mins
     ? 'Insufficient extension period'
     : 'Confirm';
 
   return (
-    <Modal
-      isOpen={restakeModalOpen}
-      onDismiss={toggleRestakeModal}
-    >
+    <Modal isOpen={restakeModalOpen} onDismiss={toggleRestakeModal}>
       <div sx={styles.container}>
         <div sx={styles.heading}>
           <h2>Restake</h2>
           <img src={images.iconDeleteRound} onClick={toggleRestakeModal} />
         </div>
-        <StayTheSameSwitchWithTitle title='Stake more HAKKA?' switchState={isKeepAmountTheSame} handleSwitchChange={handleKeepAmountTheSame} />
+        <StayTheSameSwitchWithTitle
+          title="Stake more HAKKA?"
+          switchState={isKeepAmountTheSame}
+          handleSwitchChange={handleKeepAmountTheSame}
+        />
         <div style={isKeepAmountTheSame ? { display: 'none' } : {}}>
           <div sx={styles.hakkaBalanceContainer}>
             <span>Amount</span>
@@ -138,10 +176,18 @@ const RestakeModal = ({ vaults, index, account, chainId }: RestakeModalInterface
           </div>
         </div>
         <hr sx={styles.hr} />
-        <StayTheSameSwitchWithTitle isDisable={timeLeft === 0} title='Adjust period?' switchState={isKeepPeriodTheSame} handleSwitchChange={handleKeepPeriodTheSame} />
+        <StayTheSameSwitchWithTitle
+          isDisable={timeLeft === 0}
+          title="Adjust period?"
+          switchState={isKeepPeriodTheSame}
+          handleSwitchChange={handleKeepPeriodTheSame}
+        />
         <div style={isKeepPeriodTheSame ? { display: 'none' } : {}}>
           <LockPeriodOptions onChange={setPeriod} timeLeft={timeLeft} />
-          <p sx={styles.textHint}> You can only reselect a longer duration than the left period.</p>
+          <p sx={styles.textHint}>
+            {' '}
+            You can only reselect a longer duration than the left period.
+          </p>
         </div>
         <hr sx={styles.hr} />
         <h4 sx={styles.receiveShakkaTitle}>Additional sHAKKA you get...</h4>
@@ -152,16 +198,23 @@ const RestakeModal = ({ vaults, index, account, chainId }: RestakeModalInterface
             total={parseFloat(receivedSHakkaAmount)}
           />
         </div>
-        <MyButton 
-          onClick={restake} 
-          styleKit='green' 
+        <RestakeButton
+          onClick={restake}
+          styleKit="green"
           disabled={isDisable}
+          isDisabledWhenNotPrepared={false}
+          isConnected={!!account}
+          connectWallet={toggleWalletModal}
+          isApproved={approveState === ApprovalState.APPROVED}
+          approveToken={approve}
+          targetNetwork={chainId}
+          isCorrectNetwork={isCorrectNetwork}
         >
           {btnContent}
-        </MyButton>
+        </RestakeButton>
       </div>
     </Modal>
   );
-}
+};
 
 export default RestakeModal;
