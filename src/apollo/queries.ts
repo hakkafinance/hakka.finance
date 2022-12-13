e8d6906804d1ff19c4fc124582671a909f19ada1
RamanYA <raman@pelith.com>
2022-12-13 17:33:39 +0800

    feat: p2e fetch data from galxy by space

--- src/apollo/queries.ts
+++ src/apollo/queries.ts
@@ -12,3 +12,19 @@ export const PROJECT_GALAXY_CAMPAIGN_INFO = gql`
     }
   }
 `
+
+export const PROJECT_GALAXY_CAMPAIGNS_INFO = gql`
+  query CheckSpaceCampaigns($account: String!) {
+    space(alias: "hakkafinance") {
+      campaigns(input: {ids: [""], types: [Drop]}) {
+         list{
+          id
+          whitelistInfo(address: $account) {
+            maxCount
+            usedCount
+          }
+        } 
+      }
+    }
+  }
+`
