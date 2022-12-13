bcefad095fab0af6ca25a242fcf1b9198b345c9a
RamanYA <raman@pelith.com>
2022-12-13 18:13:58 +0800

    adjust: update version

--- src/apollo/queries.ts
+++ src/apollo/queries.ts
@@ -1,18 +1,5 @@
 import gql from 'graphql-tag'
 
-export const PROJECT_GALAXY_CAMPAIGN_INFO = gql`
-  query CheckWhiteList($account: String!, $campaignId: ID!) {
-    campaign(id: $campaignId) {
-      whitelistInfo(address: $account) {
-        address
-        maxCount
-        usedCount
-      }
-      endTime
-    }
-  }
-`
-
 export const PROJECT_GALAXY_CAMPAIGNS_INFO = gql`
   query CheckSpaceCampaigns($account: String!) {
     space(alias: "hakkafinance") {
