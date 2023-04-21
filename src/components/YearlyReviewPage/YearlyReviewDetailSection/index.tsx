0e27760ca084b17e3d90f7b2c1b018b8f2940355
RamanYA <raman@pelith.com>
2023-04-21 16:51:52 +0800

    feat: score modal ui, html to image and change font loader

--- src/components/YearlyReviewPage/YearlyReviewDetailSection/index.tsx
+++ src/components/YearlyReviewPage/YearlyReviewDetailSection/index.tsx
@@ -8,9 +8,10 @@ import withWrongNetworkCheckWrapper from '../../../hoc/withWrongNetworkCheckWrap
 import withConnectWalletCheckWrapper from '../../../hoc/withConnectWalletCheckWrapper';
 import { MyButton } from '../../Common';
 import { ChainId, ChainNameWithIcon } from '../../../constants';
-import { useWalletModalToggle } from '../../../state/application/hooks';
+import { useWalletModalToggle, useYearlyReviewScoreModalToggle } from '../../../state/application/hooks';
 import ReviewItem from '../ReviewItem';
 import images from '../../../images';
+import ScoreModal from '../ScoreModal';
 
 // TODO: mock data
 const MOCK_DATA = [
@@ -18,25 +19,29 @@ const MOCK_DATA = [
     title: 'Your total transaction amount and products you have used on Hakka last year:',
     icon: images.iconReview1,
     performance: 'IRS, IG, Farming, Staking, total for 5000 USD',
-    comment: 'What a productive farmer! You are in the top 1% users!'
+    comment: 'What a productive farmer! You are in the top 1% users!',
+    shortContent: 'Used IRS, IG, Farming, Staking',
   },
   {
     title: 'Gas you have burned for Hakka last year:',
     icon: images.iconReview2,
     performance: 'Ethereum: 1.2 ether, Polygon: 0.8 matic',
-    comment: 'Such a hard worker! Your activity requires more gas than 90% of users.'
+    comment: 'Such a hard worker! Your activity requires more gas than 90% of users.',
+    shortContent: 'Used 1.2 ETH and 5.8 Matic as gas',
   },
   {
     title: 'OATs you have claimed from Hakka last year:',
     icon: images.iconReview3,
     performance: 'x of 33 OATs',
-    comment: 'Our respects to you, loyal Hakkafam!'
+    comment: 'Our respects to you, loyal Hakkafam!',
+    shortContent: 'Got 2 OATs',
   },
   {
     title: 'Your first Hakka product:',
     icon: images.iconReview4,
     performance: 'IRS, 2020/03/02',
-    comment: 'Thanks for your loyalty and patience dear Hakka rancher!'
+    comment: 'Thanks for your loyalty and patience dear Hakka rancher!',
+    shortContent: '2020/04/01 was my first day meeting Hakka'
   },
 ]
 
@@ -47,6 +52,7 @@ const YearlyReviewDetailSection = () => {
   const supportedChain = Object.keys(_omit(ChainNameWithIcon, [ChainId.KOVAN, ChainId.RINKEBY])).map((ele) => parseInt(ele));
   const isCorrectNetwork = chainId ? supportedChain.includes(chainId) : false;
   const CountScoreButton = withWrongNetworkCheckWrapper(withConnectWalletCheckWrapper(MyButton));
+  const toggleScoreModal = useYearlyReviewScoreModalToggle();
 
   return (
     <div>
@@ -65,7 +71,7 @@ const YearlyReviewDetailSection = () => {
       </div>
       <div sx={styles.countScoreButton}>
         <CountScoreButton
-          onClick={() => {}} 
+          onClick={toggleScoreModal} 
           styleKit='green'
           isConnected={isConnected}
           connectWallet={toggleWalletModal}
@@ -75,6 +81,7 @@ const YearlyReviewDetailSection = () => {
           Count your score
         </CountScoreButton>
       </div>
+      <ScoreModal p2eLevel='1' performanceList={MOCK_DATA} />
     </div>
   )
 }
